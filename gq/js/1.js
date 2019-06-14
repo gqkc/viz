5d49a3e9c2655a86d5448328f6eb9880b9f6e184
Guillaume Quispe <guillaume.quispe@telecom-paristech.fr>
2019-06-14 23:09:35 +0200

    correction gdp file

--- gq/js/1.js
+++ gq/js/1.js
@@ -5,30 +5,32 @@ var svg = d3.select("svg"),
 
 // Add X axis
 var x = d3.scaleLinear()
-    .domain([0, 40])
+    .domain([0, 100])
     .range([0, width]);
-/* svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
+ svg.append("g")
+    //.attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
 
   // Add Y axis
   var y = d3.scaleLinear()
-    .domain([0, 400000])
-    .range([ height, 0]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
+    .domain([0, 30000])
+    .range([ height,0]);
 
-*/
+  svg.append("g")
+    //  .attr("transform", "translate("+width+",0)")
+   .call(d3.axisLeft(y));
+
+   var z = d3.scaleLinear()
+    .domain([0, 1])
+    .range([ 0,0.5]);
+
+
 // Map and projection
-var path = d3.geoPath();
-var projection = d3.geoMercator()
-    .scale(70)
-    .center([0, 20])
-    .translate([width / 2, height / 2]);
 
 // Data and color scale
 var data_gdp = d3.map();
 var data_exp = d3.map();
+var data_hiv = d3.map();
 
 var colorScale = d3.scaleThreshold()
     .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
@@ -36,7 +38,8 @@ var colorScale = d3.scaleThreshold()
 
 // Load external data and boot
 d3.queue()
-    .defer(d3.csv, "../data/gdp.csv", function(d) {
+    .defer(d3.csv, "../data/gdp2.csv", function(d) {
+   //console.log(d["1980"])
         data_gdp.set(d.name, +d["1980"]);
         return d
     })
@@ -44,11 +47,16 @@ d3.queue()
         data_exp.set(d.name, +d["1980"]);
         return d
     })
+    .defer(d3.csv, "../data/adults_with_hiv_percent_age_15_49.csv", function(d) {
+    //console.log(d["1991"])
+        data_hiv.set(d.country, d["1991"]);
+        return d
+    })
     .await(ready);
 
-function ready(error, gdp, exp) {
+function ready(error, gdp, exp, hiv) {
     // Add dots
-    console.log(gdp)
+   // console.log("hiv",data_hiv)
     //console.log(data_gdp)
     var svg = d3.select("#my_dataviz")
         .append("svg")
@@ -62,12 +70,19 @@ function ready(error, gdp, exp) {
         .enter()
         .append("circle")
         .attr("cx", function(d) {
-            console.log(data_exp.get(d['name']));
-            return data_exp.get(d['name']);
+           if (data_exp.get(d['name'])!= undefined)
+            return x(data_exp.get(d['name']));
         })
         .attr("cy", function(d) {
-            return data_gdp.get(d['name']);
+            if (data_gdp.get(d['name'])!= undefined)
+            return y(data_gdp.get(d['name']));
         })
-        .attr("r", 1.5)
+        .attr("r", function(d) {
+        //console.log(data_hiv.get(d['name']))
+            if (data_hiv.get(d['name'])== undefined){
+                //console.log(data_hiv.get(d['name']))
+                return 0.01
+                }
+            return z(data_hiv.get(d['name']))})
         .style("fill", "#69b3a2")
 }
